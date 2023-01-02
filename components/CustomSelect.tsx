663e8d48f231d4b1f26e3650f71c43a8067f5b8f
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-01-02 14:37:14 +0800

    Implement formik integration

--- components/CustomSelect.tsx
+++ components/CustomSelect.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Listbox } from "@headlessui/react";
+import cn from "classnames";
 //👇 generic type for select option
 export type CustomSelectOption<T> = {
   label: string;
@@ -19,15 +20,35 @@ const CustomSelect = <T,>(props: CustomSelect<T>) => {
   const selectedItem = options.find((o) => o.value === props.value);
   const label = selectedItem?.label ?? "Select Option...";
   return (
-    <Listbox value={props.value} onChange={props.onChange}>
-      <Listbox.Button>{label}</Listbox.Button>
-      <Listbox.Options>
-        {options.map((option, i) => (
-          <Listbox.Option key={i} value={option.value}>
-            {option.label}
-          </Listbox.Option>
-        ))}
-      </Listbox.Options>
+    <Listbox value={props.value} onChange={props.onChange} as={Fragment}>
+      <div className={"dropdown dropdown-end w-full"}>
+        <Listbox.Button className="btn btn-outline w-full relative no-animation normal-case">
+          {label}
+        </Listbox.Button>
+        <Listbox.Options
+          className={cn({
+            "dropdown-content menu": true,
+            "p-2 shadow-lg bg-base-100 rounded-box w-56": true,
+          })}
+        >
+          {options.map((option, i) => (
+            <Listbox.Option key={i} value={option.value}>
+              {/* 👇 Use render props to get active, disabled and selected state */}
+              {({ active, disabled, selected }) => (
+                <button
+                  className={cn({
+                    active: selected,
+                    "btn-disabled": disabled,
+                    "bg-primary/80 text-primary-content": active,
+                  })}
+                >
+                  {option.label}
+                </button>
+              )}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </div>
     </Listbox>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,38 @@
-import CustomSelect from "components/CustomSelect";
+import FormikSelect from "components/FormikSelect";
+import { Form, Formik } from "formik";
 import { NextPage } from "next";
-import React, { useState } from "react";
-
+import React from "react";
 const HomePage: NextPage = () => {
-  const [val, setVal] = useState(0);
   return (
     <div className={"container"}>
       <div className="grid place-content-center min-h-screen">
         <div>
           <h1>Welcome to Formik Select Example</h1>
-          <CustomSelect
-            options={[
-              {
-                label: "$1000",
-                value: 1000,
-              },
-              {
-                label: "$2000",
-                value: 2000,
-              },
-            ]}
-            value={val}
-            onChange={(value) => setVal(value)}
-          />
+          <Formik
+            onSubmit={(values) => alert(JSON.stringify(values, null, 4))}
+            initialValues={{
+              salary: 0,
+            }}
+          >
+            <Form className="flex flex-col gap-2">
+              <FormikSelect
+                name="salary"
+                options={[
+                  {
+                    label: "$1000",
+                    value: 1000,
+                  },
+                  {
+                    label: "$2000",
+                    value: 2000,
+                  },
+                ]}
+              />
+              <button className="btn" type="submit">
+                Submit Form
+              </button>
+            </Form>
+          </Formik>
         </div>
       </div>
     </div>
